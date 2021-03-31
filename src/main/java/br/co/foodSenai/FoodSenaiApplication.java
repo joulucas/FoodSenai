463293e1d87485c538beb4f45a9b1ace04bf08b8
Joubert Santos <joubert.santos@mttechne.com.br>
2021-03-31 19:17:34 -0300

    estrutura

---
+++ src/main/java/br/co/foodSenai/FoodSenaiApplication.java
@@ -0,0 +1,13 @@
+package br.co.foodSenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodSenaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/co/foodSenai/FoodSenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.co.foodSenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
